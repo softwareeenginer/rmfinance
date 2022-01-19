1a3d67c2db2e41805dd9178d94e95c15e601f3e2
Mehmet Ali Yılgın <57998586+softwareeenginer@users.noreply.github.com>
2022-01-20 00:43:47 +0300

    new packages.
    "react-native-safe-area-context": "3.3.2",
    "react-native-svg": "12.1.1",

---
+++ App.tsx
@@ -0,0 +1,21 @@
+import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { NativeBaseProvider } from "native-base";
+import Home from './screens/Home';
+
+export default function App() {
+  return (
+    <SafeAreaProvider>
+      <NativeBaseProvider>
+        <Home />
+      </NativeBaseProvider>
+    </SafeAreaProvider>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+});
---
+++ screens/Home.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { View, StyleSheet, Text } from "react-native";
+
+
+const Home = () => {
+    return (
+        <View>
+            <Text></Text>
+        </View>
+    )
+}
+export default Home;
+
+const styles = StyleSheet.create({
+
+});
